15665c6eb52a2c2798046145b22dc0eae663e542
RicePandaaaa <anthony.ha.pham@gmail.com>
2026-06-14 14:16:36 -0500

    Edit method comments, start on operations

--- src/matrix.c
+++ src/matrix.c
@@ -11,11 +11,11 @@
 Matrix *matrix_create(int rows, int cols) {
     /*
      * Parameters:
-     *   rows: number of rows in the matrix
-     *   cols: number of columns in the matrix
+     *   rows (int): number of rows in the matrix
+     *   cols (int): number of columns in the matrix
      * 
      * Returns:
-     *   A pointer to the newly created matrix, or NULL if the creation failed.
+     *   (Matrix*): a pointer to the newly created matrix, or NULL if the creation failed.
      */
 
     // Edge case: invalid (non-positive) dimensions
@@ -48,7 +48,7 @@ Matrix *matrix_create(int rows, int cols) {
 void matrix_free(Matrix *m) {
     /*
      * Parameters:
-     *   m: pointer to matrix being freed
+     *   m (Matrix*): pointer to matrix being freed
      * 
      * Returns:
      *   N/A 
@@ -64,10 +64,10 @@ void matrix_free(Matrix *m) {
 Matrix *matrix_copy(const Matrix *m) {
     /*
      * Parameters:
-     *   m: matrix to make a copy of
+     *   m (Matrix*): matrix to make a copy of
      * 
      * Returns:
-     *   copy: matrix copy of m or NULL if m is invalid
+     *   copy (Matrix*): matrix copy of m or NULL if m is invalid
      */
 
     // Edge case: m is invalid
@@ -88,13 +88,13 @@ Matrix *matrix_copy(const Matrix *m) {
 bool matrix_get(double *dest, const Matrix *m, int row, int col) {
     /*
      * Parameters:
-     *   dest: variable to store the retrieved value
-     *   m: matrix to be accessed
-     *   row: row where the desired value is
-     *   col: col where the desired value is
+     *   dest (double*): variable to store the retrieved value
+     *   m    (Matrix*): matrix to be accessed
+     *   row  (int)    : row where the desired value is
+     *   col  (int): col where the desired value is
      * 
      * Returns:
-     *   bool: whether or not the value was successfully retrieved
+     *   (bool): whether or not the value was successfully retrieved
      */
 
     // Edge case: m is invalid
@@ -116,10 +116,10 @@ bool matrix_get(double *dest, const Matrix *m, int row, int col) {
 bool matrix_set(Matrix *m, int row, int col, double value) {
     /*
      * Parameters:
-     *   m: matrix to set value in
-     *   row: row to place value in
-     *   col: col to place value in
-     *   value: the value to place in m
+     *   m     (Matrix*): matrix to set value in
+     *   row   (int)    : row to place value in
+     *   col   (int)    : col to place value in
+     *   value (double) : the value to place in m
      * 
      * Returns:
      *   bool: whether or not the value was successfully set
@@ -148,8 +148,8 @@ bool matrix_set(Matrix *m, int row, int col, double value) {
 void matrix_fill(Matrix *m, double value) {
     /*
      * Parameters:
-     *   m: matrix to fill with value "val"
-     *   value: value to fill matrix "m" with
+     *   m     (Matrix*): matrix to fill with value "val"
+     *   value (double) : value to fill matrix "m" with
      * 
      * Returns:
      *   N/A
@@ -171,9 +171,9 @@ void matrix_fill(Matrix *m, double value) {
 void matrix_rand(Matrix *m, double lower, double upper) {
     /*
      * Parameters:
-     *   m: matrix to fill with random values
-     *   lower: lower bound (included) of random values
-     *   upper: upper bound (included) of random values
+     *   m     (*Matrix): matrix to fill with random values
+     *   lower (double) : lower bound (included) of random values
+     *   upper (double) : upper bound (included) of random values
      * 
      * Returns:
      *   N/A
@@ -195,4 +195,112 @@ void matrix_rand(Matrix *m, double lower, double upper) {
         double rand_value = ((double) rand() / RAND_MAX) * (upper - lower) + lower;
         m->data[index] = rand_value;
     }
+}
+
+/*
+ * Math operation methods
+ */
+
+
+Matrix *matrix_add(const Matrix* a, const Matrix *b) {
+    /*
+     * Parameters:
+     *   a (Matrix*): first matrix to be added
+     *   b (Matrix*): second matrix to be added
+     * 
+     * Returns:
+     *   result (Matrix*): result of a + b
+     */
+
+    // Edge case: either matrix is invalid
+    if (a == NULL || b == NULL) {
+        return NULL;
+    }
+
+    // Edge case: matrices aren't of same shape
+    if (a->rows != b->rows || a->cols != b->cols) {
+        return NULL;
+    }
+
+    // Create and fill new matrix
+    Matrix *result = matrix_create(a->rows, a->cols);
+
+    int mat_size = a->rows * a->cols;
+    for (int index = 0; index < mat_size; index++) {
+        result->data[index] = a->data[index] + b->data[index];
+    }
+
+    return result;
+}
+
+
+Matrix *matrix_sub(const Matrix *a, const Matrix *b) {
+    /*
+     * Parameters:
+     *   a (Matrix*): matrix to subtract from
+     *   b (Matrix*): matrix to subtract from a with
+     * 
+     * Returns:
+     *   result (Matrix*): result of a - b
+     */
+
+    // Edge case: either matrix is invalid
+    if (a == NULL || b == NULL) {
+        return NULL;
+    }
+
+    // Edge case: matrices aren't of same shape
+    if (a->rows != b->rows || a->cols != b->cols) {
+        return NULL;
+    }
+
+    // Create and fill new matrix
+    Matrix *result = matrix_create(a->rows, a->cols);
+
+    int mat_size = a->rows * a->cols;
+    for (int index = 0; index < mat_size; index++) {
+        result->data[index] = a->data[index] - b->data[index];
+    }
+
+    return result;
+}
+
+
+Matrix *mat_mult(const Matrix *a, const Matrix *b) {
+    /*
+     * Parameters:
+     *   a (Matrix*): first (left) matrix to multiply
+     *   b (Matrix*): second (right) matrix to multiply
+     * 
+     * Returns:
+     *   result (Matrix*): result of standard matrix mutliplication AB
+     */
+
+    // Edge case: either matrix is invalid
+    if (a == NULL || b == NULL) {
+        return NULL;
+    }
+
+    // Edge case: dimensions don't match
+    if (a->cols != b->rows) {
+        return NULL;
+    }
+
+    // Create the matrix
+    Matrix *result = matrix_create(a->rows, b->cols);
+
+    // Go through each position in the result
+    int mat_size = a->rows * b->cols;
+    for (int index = 0; index < mat_size; index++) {
+        int row_index = index / b->cols;
+        int col_index = index % b->cols;
+
+        // Note that the length of a row in a is equal to a->cols,
+        // which is also equal to the height of a col in b
+        for (int k = 0; k < a->cols; k++) {
+            result->data[index] += a->data[row_index * a->cols + k] * b->data[k * b->cols + col_index];
+        }
+    }
+
+    return result;
 }
